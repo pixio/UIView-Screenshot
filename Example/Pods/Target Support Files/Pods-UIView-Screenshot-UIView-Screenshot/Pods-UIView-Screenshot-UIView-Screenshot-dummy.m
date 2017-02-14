692e24b58153222dab9f97183c3967b2ed9f0d50
Michael Biggs <mbiggs@pixio.com>
2017-02-14 10:18:56 -0700

    Updated podfile to work with CocoaPods 1.0 and updated project settings

--- Example/Pods/Target Support Files/Pods-UIView-Screenshot-UIView-Screenshot/Pods-UIView-Screenshot-UIView-Screenshot-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_UIView_Screenshot_UIView_Screenshot : NSObject
-@end
-@implementation PodsDummy_Pods_UIView_Screenshot_UIView_Screenshot
-@end
